13eb053b5295b76a257b8f9b64391cd8653a1ae4
Justin Collins <jcollins@attinteractive.com>
2012-01-23 16:26:47 -0800

    Output fixed, new, existing warning labels

--- lib/guard/brakeman.rb
+++ lib/guard/brakeman.rb
@@ -38,7 +38,7 @@ module Guard
     def run_on_change(paths)
       puts "rescanning #{paths}, running all checks"
       report = ::Brakeman::rescan(@tracker, paths)
-      print_failed(report)
+      print_changed(report)
       throw :task_has_failed if report.any_warnings?
     end
 
@@ -47,6 +47,31 @@ module Guard
     def print_failed report
       puts "\n------ brakeman warnings --------\n"
       puts report.all_warnings.sort_by { |w| w.confidence }
+    end
+
+    def print_changed report
+      puts "\n------ brakeman warnings --------\n"
+
+      unless report.fixed_warnings.empty?
+        puts "#{report.fixed_warnings.length} fixed warnings:"
+        puts report.fixed_warnings.sort_by { |w| w.confidence }
+        puts
+      end
+
+      unless report.new_warnings.empty?
+        puts "#{report.new_warnings.length} new warnings:"
+        puts report.new_warnings.sort_by { |w| w.confidence }
+        puts
+      end
+
+      existing = report.all_warnings.select do |w| 
+        not report.new_warnings.include? w
+      end
+
+      unless existing.empty?
+        puts "#{existing.length} previous warnings:"
+        puts existing.sort_by { |w| w.confidence }
+      end
     end
   end
 end
