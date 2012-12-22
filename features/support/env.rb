6c9bd82686fc98ee6da44f8f7ea82596c9f0d60c
oreoshake <neil.matatall@gmail.com>
2012-12-21 17:28:40 -0800

    Fix JRuby maybe?

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,5 +1,6 @@
 require File.expand_path('../../../lib/guard/brakeman',  __FILE__)
 require 'aruba/cucumber'
+require 'aruba/java' if RUBY_PLATFORM == 'java'
 
 Before do
   @aruba_timeout_seconds = 20
