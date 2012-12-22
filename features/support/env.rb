9813bddfc5bb05a33832a34146652cf7a593fe4d
oreoshake <neil.matatall@gmail.com>
2012-12-21 17:34:34 -0800

    Fix JRuby maybe? Part 2

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,11 +1,16 @@
 require File.expand_path('../../../lib/guard/brakeman',  __FILE__)
 require 'aruba/cucumber'
-require 'aruba/java' if RUBY_PLATFORM == 'java'
+
+Aruba.configure do |config|
+  config.before_cmd do |cmd|
+    set_env('JRUBY_OPTS', "-X-C #{ENV['JRUBY_OPTS']}") # disable JIT since these processes are so short lived
+    set_env('JAVA_OPTS', "-d32 #{ENV['JAVA_OPTS']}") # force jRuby to use client JVM for faster startup times
+  end
+end if RUBY_PLATFORM == 'java'
 
 Before do
   @aruba_timeout_seconds = 20
 end
-
 
 After do
   content = <<-EOF
